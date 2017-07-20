612197436cb3b94a92e5a7e737a757230d50a186
Jordan Hudgens <kalynavery@live.com>
2017-07-20 15:47:56 +0000

    Fixed renderer.rb

--- lib/devcamp_view_tool_avery.rb
+++ lib/devcamp_view_tool_avery.rb
@@ -1,10 +1,7 @@
 require "devcamp_view_tool_avery/version"
 require "devcamp_view_tool_avery/renderer"
 
-  module DevcampViewToolAvery
-    class Renderer
-    def self.copyright name, msg
-      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-    end
-    end
-  end
+module DevcampViewToolAvery
+  
+end
+  
